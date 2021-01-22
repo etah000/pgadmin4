ab7e3c5e669a7c2b8f55044d00cee6d2ea822ba7
Mr.General <Mr.General@gmail.com>
2021-01-22 16:34:29 +0800

    update Table(add constraints sql template)

--- web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/tables/sql/default/create.sql
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/tables/sql/default/create.sql
@@ -3,13 +3,14 @@
 {% import 'macros/variable.macros' as VARIABLE %}
 {% import 'columns/macros/security.macros' as COLUMN_SECLABEL %}
 {% import 'columns/macros/privilege.macros' as COLUMN_PRIVILEGE %}
-{% import 'tables/sql/macros/constraints.macro' as CONSTRAINTS %}
+{# import 'tables/sql/macros/constraints.macro' as CONSTRAINTS #}
 {#===========================================#}
 {#====== MAIN TABLE TEMPLATE STARTS HERE ======#}
 {#===========================================#}
 
 {% from 'tables/sql/macros/column.macro' import COLUMNS %}
-{% from 'tables/sql/macros/index.macro' import INDEX %}
+{% from 'tables/sql/macros/index.macro' import INDEXES %}
+{% from 'tables/sql/macros/constraint.macro' import CONSTRAINTS %}
 {% from 'tables/sql/macros/engine.macro' import ENGINE %}
 
 
@@ -29,10 +30,13 @@ CREATE TABLE {{did}}.{{conn|qtIdent(data.name)}}{% if data.cluster %} ON CLUSTER
 {% endif %}
 
 {### Add indexes ###}
-{%- if data.indexes and data.indexes|length > 0 %},
-{% for idx in data.indexes -%}
-{{ INDEX(idx) }}{% if not loop.last %},{% endif %}
-{%- endfor %}
+{%- if data.indexes and data.indexes|length > 0 %}
+{{ INDEXES(data) }}
+{% endif %}
+
+{### Add constraints ###}
+{%- if data.constraints and data.constraints|length > 0 %}
+{{ CONSTRAINTS(data) }}
 {% endif %}
 
 {% if data.columns|length > 0 %}){% endif %}
