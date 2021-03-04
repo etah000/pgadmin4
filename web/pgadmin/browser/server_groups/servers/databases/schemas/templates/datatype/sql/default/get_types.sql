4419ee7a48d7b2fca5c211eda31f2dd73892c0b9
Mr.General <Mr.General@gmail.com>
2021-03-04 10:13:39 +0800

    update DataTypeReader(add order by)

--- web/pgadmin/browser/server_groups/servers/databases/schemas/templates/datatype/sql/default/get_types.sql
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/templates/datatype/sql/default/get_types.sql
@@ -14,3 +14,4 @@ WHERE
     AND name not in ('AggregateFunction','Array','LowCardinality','MultiPolygon','Nested','Nothing','Nullable','Point','Polygon','Ring','SimpleAggregateFunction','Tuple')
     AND name not like 'Interval%'
     AND name not in ('Enum8','Enum16','Enum','UInt256','Int256','Decimal256','Int128')
+ORDER BY name
