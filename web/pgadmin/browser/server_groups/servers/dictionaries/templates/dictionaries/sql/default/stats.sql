ff5213c851435a0c7f42ef38876d8cd4d65d18a6
jetgm <jetgm@163.com>
2021-03-19 18:36:01 +0800

    update translations

--- web/pgadmin/browser/server_groups/servers/dictionaries/templates/dictionaries/sql/default/stats.sql
+++ web/pgadmin/browser/server_groups/servers/dictionaries/templates/dictionaries/sql/default/stats.sql
@@ -1,7 +1,7 @@
 {### SQL to fetch dictionary object stats ###}
 select
-       name,
-       query_count,
-       loading_duration,
-       last_exception
+       name as {{ conn|qtIdent(_('Name')) }},
+       query_count as {{ conn|qtIdent(_('Query Count')) }},
+       loading_duration as {{ conn|qtIdent(_('Loading Duration')) }},
+       last_exception  as {{ conn|qtIdent(_('Last Exception')) }}
 from system.dictionaries;
--- web/pgadmin/browser/server_groups/servers/tablespaces/templates/tablespaces/sql/default/stats.sql
+++ web/pgadmin/browser/server_groups/servers/tablespaces/templates/tablespaces/sql/default/stats.sql
@@ -1,8 +1,8 @@
 {### SQL to fetch tablespace object stats ###}
-SELECT name as Name,
-        concat(toString(round(total_space/(1024*1024*1024))),'G') as Total_Size
-        ,concat(toString(round(free_space/(1024*1024*1024))),'G') as Free_Size
-        ,concat(toString(round(keep_free_space/(1024*1024*1024))),'G') as Keep_Free_Size
+SELECT name as {{ conn|qtIdent(_('Name')) }},
+        concat(toString(round(total_space/(1024*1024*1024))),'G') as {{ conn|qtIdent(_('Total Size')) }},
+        concat(toString(round(free_space/(1024*1024*1024))),'G') as {{ conn|qtIdent(_('Free Size')) }},
+        concat(toString(round(keep_free_space/(1024*1024*1024))),'G') as {{ conn|qtIdent(_('Keep Free Size')) }}
 FROM system.disks;
 
 
